f267ddf61e9c5e25d470aec3ae2c246652985232
Alan932 <47587128+Alan932@users.noreply.github.com>
2021-12-21 03:12:26 +1100

    created template for the monthly expense tab

---
+++ src/Components/Tabs/MonthlyExpenseTab.js
@@ -0,0 +1,15 @@
+import './MonthlyExpenseTab.css';
+
+const MonthlyExpenseTab = () => {
+    return (
+        // <div className='output'>
+        //     Monthly Expense
+        // </div>
+
+        <section >
+            &lt;add content here&gt;
+        </section>
+    )
+};
+
+export default MonthlyExpenseTab;

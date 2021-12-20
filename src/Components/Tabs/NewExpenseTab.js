12d470d3c6c472445a2843ebbee67e35ac0763d4
Alan932 <47587128+Alan932@users.noreply.github.com>
2021-12-20 17:21:34 +1100

    initial start; working on creating tabs

---
+++ src/Components/Tabs/NewExpenseTab.js
@@ -0,0 +1,12 @@
+import './NewExpenseTab.css';
+
+const NewExpenseTab = () => {
+    return (
+        <div className="NewExpenseTab">
+            <p>First Tab!! Hurray!!</p>
+            {/* First tab content will go here */}
+        </div>
+    );
+};
+
+export default NewExpenseTab;

d4b615404bc3a90627c327f00c009164115409e0
Alan932 <47587128+Alan932@users.noreply.github.com>
2021-12-21 03:13:17 +1100

    added css file for Tabs and finished rough outline for tabs

--- src/Components/Tabs/Tab.js
+++ src/Components/Tabs/Tab.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import NewExpenseTab from "./NewExpenseTab";
+import MonthlyExpenseTab from "./MonthlyExpenseTab";
+import './Tab.css';
+
 const Tabs = () => {
-  return (
-    <div className="Tabs">
-      {/* Tab nav */}
-      <ul className="nav">
-        <li>Tab 1</li>
-        <li>Tab 2</li>
-      </ul>
-      <div className="outlet">
-        {/* content will be shown here */}
-      </div>
-    </div>
-  );
+    const [activeTab, setActiveTab] = useState("NewExpenseTab");
+
+    const switchNewExpenseTab = () => {
+        setActiveTab("NewExpenseTab");
+    };
+
+    const switchMonthlyExpenseTab = () => {
+        setActiveTab("MonthlyExpenseTab");
+    };
+
+    return (
+        <div className="Tab">
+            {/* Tab nav */}
+            <ul className="nav">
+                <li className={activeTab === "NewExpenseTab" ? "active" : ""} onClick={switchNewExpenseTab}>
+                    New Expense
+                </li>
+                <li className={activeTab === "MonthlyExpenseTab" ? "active" : ""} onClick={switchMonthlyExpenseTab}>
+                    Monthly Spend
+                </li>
+            </ul>
+            <div>
+                {/* content will be shown here */}
+                {activeTab === "NewExpenseTab" ? <NewExpenseTab /> : <MonthlyExpenseTab />}
+            </div>
+        </div>
+    );
 };
+
 export default Tabs;

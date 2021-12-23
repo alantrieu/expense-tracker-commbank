566021dda35c43cdb0735b095ed3165aa713ffcd
Alan932 <47587128+Alan932@users.noreply.github.com>
2021-12-23 19:06:51 +1100

    added capability to display current expenses in the Monthly Spend tab

---
+++ src/Components/Tabs/ExpensesList.js
@@ -0,0 +1,25 @@
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+    // TODO: delete 
+    if (props.myExpenses.length === 0) {
+        return <h2 className='expenses-list__fallback'>Found no expenses.</h2>
+    }
+
+    return (
+        <ul className='expenses-list'>
+            {props.myExpenses.map(expense => (
+                <ExpenseItem 
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                    category={expense.category}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default ExpensesList;

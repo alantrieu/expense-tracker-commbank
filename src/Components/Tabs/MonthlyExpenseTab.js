c9100ee4e5d6213f840a8692250fe35a1c5b0235
Alan932 <47587128+Alan932@users.noreply.github.com>
2022-01-28 00:13:33 +1100

    successfully added a chart

--- src/Components/Tabs/MonthlyExpenseTab.js
+++ src/Components/Tabs/MonthlyExpenseTab.js
@@ -1,12 +1,28 @@
+import { React, useState } from 'react';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import ExpensesFilter from './ExpensesFilter';
+import AuthContext from '../../Context/auth-context';
 import './MonthlyExpenseTab.css';
 
 const MonthlyExpenseTab = (props) => {
+    const [chartYear, setChartYear] = useState('2022');
+
+    const chartChangeHandler = (newYear) => {
+        setChartYear(newYear);
+    };
+
+    const filteredExpenses = props.myExpenses.filter(expense => {
+        return expense.date.getFullYear().toString() === chartYear;
+    });
+
     return (
-        <div >
-            <ExpensesList myExpenses={props.myExpenses} />
-        </div>
-    )
+        <AuthContext.Provider value={{currMonth: 'Jan',}} >
+            <ExpensesFilter selected={chartYear} onChangeFilter={chartChangeHandler}/>
+            <ExpensesChart expense={filteredExpenses} />
+            <ExpensesList myExpenses={filteredExpenses} />
+        </AuthContext.Provider>
+    );
 };
 
 export default MonthlyExpenseTab;

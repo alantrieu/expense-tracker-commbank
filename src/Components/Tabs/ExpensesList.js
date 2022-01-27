eb9de8bf2a4e2d6b10a508f0f30f9933a09263e0
Alan932 <47587128+Alan932@users.noreply.github.com>
2022-01-28 01:35:45 +1100

    implemented context hook and selecting chart month

--- src/Components/Tabs/ExpensesList.js
+++ src/Components/Tabs/ExpensesList.js
@@ -1,32 +1,40 @@
 import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import AuthContext from '../../Context/auth-context';
 import './ExpensesList.css';
 
 const ExpensesList = (props) => {
-
+    const ctx = useContext(AuthContext);
+    
     if (props.myExpenses.length === 0) {
         return <h2 className='expenses-list__fallback'>Found no expenses.</h2>
     }
 
+    // filter expenses by currently chosen month
+    const monthlyExpenses = props.myExpenses.filter(expense => {
+        return expense.date.getMonth() === ctx.currMonth;
+    });
+
     var total = 0;
-    for (const expense of props.myExpenses) {
+    for (const expense of monthlyExpenses) {
         total += expense.amount;
     }
 
     return (
         <React.Fragment>
-        <div>{total}</div>
-        <ul className='expenses-list'>
-            {props.myExpenses.map(expense => (
-                <ExpenseItem 
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-                category={expense.category}
-                />
+            <Card className="expense-title">Monthly Spending: ${total}</Card>
+            <ul className='expenses-list'>
+                {monthlyExpenses.map(expense => (
+                    <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                        category={expense.category}
+                    />
                 ))}
-        </ul>
+            </ul>
         </React.Fragment>
     );
 };
